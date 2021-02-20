22297be2d160d5114b50a1ad695d01af0404eddc
Takayuki Miyahara <voyager.3taka28@gmail.com>
2021-02-20 17:11:57 +0900

    feat: boolean

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -359,3 +359,34 @@ func testInfixExpression(t *testing.T, exp ast.Expression, left interface{}, ope
 
 	return true
 }
+
+func TestBooleanExpression(t *testing.T) {
+	input := "TRUE"
+
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseBoolean()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("program has not enough statements. got=%d", len(program.Statements))
+	}
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements[0] is not ast.ExpressionStatement. got=%T", program.Statements[0])
+	}
+
+	literal, ok := stmt.Expression.(*ast.IntegerLiteral)
+	if !ok {
+		t.Fatalf("exp not *ast.IntegerLiteral. got=%T", stmt.Expression)
+	}
+	if literal.TokenLiteral() != "5" {
+		t.Errorf("literal.Value not %d, got=%d", 5, literal.Value)
+	}
+	if literal.TokenLiteral() != "5" {
+		t.Errorf("literal.TokenLiteral not %s, got=%s", "5", literal.TokenLiteral())
+
+	}
+
+}
